43688164f49ad5d7a9ab62c221deb5554e0c07f3
devesh-y <ydevesh136@gmail.com>
2024-01-01 03:12:13 +0530

    loading screen

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,10 +1,19 @@
 import backgroundImage from "../assets/background.png"
 import logo from "../assets/goodspaceLogo.png"
 import {Toaster} from "@/components/ui/sonner.tsx";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import {getCookie} from "@/utilities/getSetCookie.ts";
 import {Outlet, useNavigate} from "react-router-dom";
+import {Loader2} from "lucide-react";
 export const HomePage=()=>{
+    const [loading,setLoading]=useState(true);
+    useEffect(() => {
+        const dummy=document.createElement("img");
+        dummy.src=backgroundImage;
+        dummy.addEventListener("load",()=>{
+            setLoading(false);
+        })
+    }, []);
     const navigate=useNavigate();
     useEffect(() => {
         const cookie=getCookie();
@@ -15,9 +24,20 @@ export const HomePage=()=>{
             navigate("/login",{replace:true})
         }
     }, [navigate]);
-    return <div className={"h-screen w-screen bg-cover flex flex-col bg-fixed overflow-auto" } style={{ backgroundImage: `url(${backgroundImage})` }}>
-        <div className={"pl-20 pt-2 pb-2 bg-transparent backdrop-blur-sm"}><img alt={"logo"} src={logo} width={200} height={200}/></div>
-        <Outlet/>
-        <Toaster/>
-    </div>
+    return <>
+        {loading ? <div className={"h-screen w-screen flex justify-center items-center"}>
+                <Loader2 size={40} color={"black"} className={"animate-spin"}/>
+            </div> :
+            <div className={"h-screen w-screen bg-cover flex flex-col bg-fixed overflow-auto"}
+                 style={{backgroundImage: `url(${backgroundImage})`}}>
+                <div className={"pl-20 pt-2 pb-2 bg-transparent backdrop-blur-sm"}><img alt={"logo"} src={logo}
+                                                                                        width={200} height={200}/></div>
+                <Outlet/>
+                <Toaster/>
+            </div>
+        }
+
+    </>
+
+
 }
