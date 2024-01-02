dc54d6969702c00c377387ab078857e39149d5e8
devesh-y <ydevesh136@gmail.com>
2024-01-02 14:25:59 +0530

    bg fixed removed

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -28,7 +28,7 @@ export const HomePage=()=>{
         {loading ? <div className={"h-[100svh] w-screen flex justify-center items-center"}>
                 <Loader2 size={40} color={"black"} className={"animate-spin"}/>
             </div> :
-            <div className={"h-screen w-screen bg-cover flex flex-col bg-fixed overflow-auto"}
+            <div className={"h-screen w-screen bg-cover flex flex-col overflow-auto"}
                  style={{backgroundImage: `url(${backgroundImage})`}}>
                 <div className={"pl-20 pt-2 pb-2 bg-transparent backdrop-blur-sm"}><img alt={"logo"} src={logo}
                                                                                         width={200} height={200}/></div>
