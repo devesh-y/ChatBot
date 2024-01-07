be10d039e44a20c3e8b7acd37d0383a8fc49cb73
devesh-y <ydevesh136@gmail.com>
2024-01-07 22:22:26 +0530

    background changes

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,4 +1,4 @@
-import backgroundImage from "../assets/background.svg"
+import backgroundImage from "../assets/backGroundImage.svg"
 import {Toaster} from "@/components/ui/sonner.tsx";
 import {useEffect} from "react";
 import {getCookie} from "@/utilities/getSetCookie.ts";
